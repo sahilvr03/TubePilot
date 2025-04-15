8c3f21e47199962049fed84fb4bf59429852755f
vijay rajesh <ewe111.vijay@gmail.com>
2025-04-15 10:40:38 +0500

    ..

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Cmart City Lab",
+  title: "Smart City Lab",
   description: "National Centre Of Artificial Intelligence",
 };
 
