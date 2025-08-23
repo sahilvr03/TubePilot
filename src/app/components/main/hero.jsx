ad395665e5da06484e4f991d620fcf7e1169a195
Hashim Hasan <141049257+asimvijay@users.noreply.github.com>
2025-08-24 04:05:04 +0500

    ..

--- src/app/components/main/hero.jsx
+++ src/app/components/main/hero.jsx
@@ -92,7 +92,7 @@ const DotLottieReact = dynamic(
 
 export default function HeroSection() {
   const isMobile = useMediaQuery("(max-width: 640px)");
-  const duration = isMobile ? 15 : 35;
+  const duration = isMobile ? 15 : 30;
 
   return (
     <section
